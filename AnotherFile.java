d1bd2685cb9deba0d7b46f20fcd66ddd292fabdc
RichardKobay <ricardo.roel110@gmail.com>
2024-01-11 19:16:22 -0600

    Added a new file and changed README.md

---
+++ AnotherFile.java
@@ -0,0 +1,6 @@
+public class AnotherFile {
+    public static void main(String[] args) {
+        System.out.println("Hello world from another version of git");
+        System.err.println(":3");
+    }
+}
